f7cf14dd7ea3e94238b24834a9ab911a534d2ed7
Andres Parra <cifucar79@gmail.com>
2023-07-17 11:15:18 -0500

    addEventListener

--- mokepon.js
+++ mokepon.js
@@ -1,6 +1,24 @@
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+
+}
 
 function seleccionarMascotaJugador(){
-    alert("mascota")
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    if (inputHipodoge.checked){
+        alert("hipodoge seleccionado")
+    }else if(inputCapipepo.checked){
+        alert("seleccionaste a capipepo")
+    }else if(inputRatigueya.checked){
+        alert("ratigueya seleccionada")
+    }
+    else{
+        alert("Debes seleccionar algo")
+    }
 }
+
+window.addEventListener('load', iniciarJuego)
