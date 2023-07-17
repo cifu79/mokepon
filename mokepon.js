e80ddde91b241b2cc319d0cd10574ebce5653f38
Andres Parra <cifucar79@gmail.com>
2023-07-17 09:17:27 -0500

    ubicando la etiqueta de JavaScript

---
+++ mokepon.js
@@ -0,0 +1,6 @@
+let botonMascotaJugador = document.getElementById('boton-mascota')
+botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+
+function seleccionarMascotaJugador(){
+    alert("mascota")
+}
